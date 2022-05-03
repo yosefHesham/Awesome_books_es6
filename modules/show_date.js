ec17196bcaa6eea885c3eab13655bf11da8f7059
yosefHesham <yousefmobdev@gmail.com>
2022-05-03 17:51:03 +0200

    fix css linters

--- modules/show_date.js
+++ modules/show_date.js
@@ -1,10 +1,10 @@
-import { DateTime } from "./luxon.js";
+import { DateTime } from './luxon.js';
 
-const dateElement = document.querySelector(".date");
+const dateElement = document.querySelector('.date');
 
 const showDate = () => {
   dateElement.innerHTML = `${DateTime.now().toLocaleString(
-    DateTime.DATETIME_MED
+    DateTime.DATETIME_MED,
   )}`;
 };
 
